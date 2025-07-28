48bcd63c91859c79a016bf1941fc6048ccb68269
maverickOG <heysajit@gmail.com>
2025-07-28 22:45:46 +0530

    feat: integrate full landing page layout with Header, Hero, Footer and WhyUs components

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,19 @@
 import ScrapPricingPage from "../components/Prices";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import HowWeWork from "../components/HowWeWork";
+import WhyUs from "../components/WhyUs";
 
 export default function Home() {
   return (
     <main>
+      <Header />
+      <Hero />
       <ScrapPricingPage />
+      <HowWeWork />
+      <WhyUs />
+      <Footer />
     </main>
   );
 }
